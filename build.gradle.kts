bb22f0548b7e38b940ac92300a456e1662d1e864
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2019-07-19 19:48:00 -0300

    Atualizado configuração do travis

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,16 @@ buildscript {
     repositories {
         google()
         jcenter()
-        maven(url = "https://jitpack.io")
         maven(url = "https://kotlin.bintray.com/kotlinx")
         maven(url = "https://plugins.gradle.org/m2/")
+        maven(url = "https://jitpack.io")
     }
 
     dependencies {
         classpath(BuildPlugins.Dependencies.androidSupport)
         classpath(BuildPlugins.Dependencies.kotlinSupport)
+        classpath(BuildPlugins.Dependencies.cobertura)
+        classpath(BuildPlugins.Dependencies.coveralls)
     }
 }
 
@@ -17,11 +19,15 @@ allprojects {
     repositories {
         google()
         jcenter()
-        maven(url = "https://jitpack.io")
         maven(url = "https://kotlin.bintray.com/kotlinx")
+        maven(url = "https://plugins.gradle.org/m2/")
+        maven(url = "https://jitpack.io")
     }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
+apply(plugin = BuildPlugins.Ids.cobertura)
+apply(plugin = BuildPlugins.Ids.coveralls)
+
+//tasks.register("clean", Delete::class) {
+//    delete(rootProject.buildDir)
+//}
--- buildSrc/src/main/java/BuildPlugins.kt
+++ buildSrc/src/main/java/BuildPlugins.kt
@@ -6,6 +6,8 @@ object BuildPlugins {
         const val androidSupport = "com.android.tools.build:gradle:${Versions.agp}"
         const val kotlinSupport = "org.jetbrains.kotlin:kotlin-gradle-plugin:${KotlinConfig.version}"
         const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:${Versions.ktlint}"
+        const val cobertura = "net.saliman:gradle-cobertura-plugin:${Versions.cobertura}"
+        const val coveralls = "gradle.plugin.com.github.kt3k.coveralls:coveralls-gradle-plugin:${Versions.coveralls}"
     }
 
     object Ids {
@@ -13,10 +15,16 @@ object BuildPlugins {
         const val kotlinAndroid = "android"
         const val kotlinExtensions = "android.extensions"
         const val kotlinKapt = "kapt"
+        const val cobertura = "net.saliman.cobertura"
+        const val coveralls = "com.github.kt3k.coveralls"
+        const val coberturaVersion = "2.6.1"
+        const val coverallsVersion = "2.8.4"
     }
 
     private object Versions {
         const val agp = "3.4.2"
         const val ktlint = "8.1.0"
+        const val cobertura = "2.6.1"
+        const val coveralls = "2.8.4"
     }
 }
