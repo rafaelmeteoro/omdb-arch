5508964e0871b0b27f225f25cb1cefecd013b77b
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2019-07-23 16:10:41 -0300

    Atualizado configuração do travis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         getByName("debug") {
             applicationIdSuffix = ".debug"
             versionNameSuffix = "-DEBUG"
-            isTestCoverageEnabled = false
+            isTestCoverageEnabled = true
         }
         getByName("release") {
             isMinifyEnabled = true
