cf28fe3c8f1ffaf2f330523d667a392d88c9dae7
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2019-07-26 14:11:10 -0300

    Atualizado travis yml

--- emulate-ci-build.sh
+++ emulate-ci-build.sh
@@ -3,6 +3,7 @@
 set -ex
 
 ./gradlew --stop
+./gradlew clean ktlintCheck --no-daemon --stacktrace
+./gradlew clean detekt --no-daemon --stacktrace
 ./gradlew clean test --no-daemon --stacktrace
 ./gradlew clean assembleDebug -xlint --no-daemon --stacktrace
-./gradlew clean build jacocoTestReport coveralls --no-daemon --stacktrace
