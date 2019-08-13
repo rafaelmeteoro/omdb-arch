8869e866f8df53578bd31bf8a97d2fb4f807bd64
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2019-08-13 17:03:10 -0300

    Set disposer in module to not be singleton

--- shared-utilities/src/main/java/com/meteoro/omdbarch/utilities/di/ShareUtilsModule.kt
+++ shared-utilities/src/main/java/com/meteoro/omdbarch/utilities/di/ShareUtilsModule.kt
@@ -4,12 +4,10 @@ import com.meteoro.omdbarch.logger.Logger
 import com.meteoro.omdbarch.utilities.Disposer
 import dagger.Module
 import dagger.Provides
-import javax.inject.Singleton
 
 @Module
 class ShareUtilsModule {
 
     @Provides
-    @Singleton
     fun provideDisposer(logger: Logger): Disposer = Disposer(logger)
 }
