adc359d61b6b9d7ccba5e00c6de45183ab9d7e1d
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2019-11-12 18:42:25 -0300

    Update custom view

--- libraries/ui-components/src/main/java/com/meteoro/omdbarch/components/widgets/manyfacedview/view/ManyFacedView.kt
+++ libraries/ui-components/src/main/java/com/meteoro/omdbarch/components/widgets/manyfacedview/view/ManyFacedView.kt
@@ -1,12 +1,15 @@
 package com.meteoro.omdbarch.components.widgets.manyfacedview.view
 
 import android.animation.Animator
+import android.animation.AnimatorInflater
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
+import androidx.annotation.LayoutRes
 import com.meteoro.omdbarch.components.R
 import com.meteoro.omdbarch.components.widgets.manyfacedview.animator.AnimatorComposer
 
@@ -45,10 +48,76 @@ class ManyFacedView @JvmOverloads constructor(context: Context, attrs: Attribute
 
     private fun parseAttributes(attrs: AttributeSet) {
         val ta = resources.obtainAttributes(attrs, R.styleable.ManyFacedView)
+
+        val hasContentView = inflateViewForStateIfExists(ta, FacedViewState.CONTENT, R.styleable.ManyFacedView_mfv_content)
+        check(hasContentView) { "Missing view for content" }
+
+        inflateViewForStateIfExists(ta, FacedViewState.EMPTY, R.styleable.ManyFacedView_mfv_empty)
+        inflateViewForStateIfExists(ta, FacedViewState.ERROR, R.styleable.ManyFacedView_mfv_error)
+        inflateViewForStateIfExists(ta, FacedViewState.LOADING, R.styleable.ManyFacedView_mfv_loading)
+
+        if (ta.hasValue(R.styleable.ManyFacedView_mfv_state)) {
+            initialState = ta.getInt(R.styleable.ManyFacedView_mfv_state, FacedViewState.UNKNOWN)
+        }
+
+        animateTransition = ta.getBoolean(R.styleable.ManyFacedView_mfv_animateChanges, true)
+
+        val inAnimationId = ta.getResourceId(R.styleable.ManyFacedView_mfv_inAnimation, R.anim.mfv_fade_in)
+        inAnimator = AnimatorInflater.loadAnimator(context, inAnimationId)
+
+        val outAnimationId = ta.getResourceId(R.styleable.ManyFacedView_mfv_outAnimation, R.anim.mfv_fade_out)
+        outAnimator = AnimatorInflater.loadAnimator(context, outAnimationId)
+
         ta.recycle()
     }
 
     private fun setupView() {
+        val newState = if (initialState == FacedViewState.UNKNOWN) FacedViewState.CONTENT else initialState
+        setState(newState)
+    }
 
+    private fun inflateViewForStateIfExists(ta: TypedArray, @FacedViewState state: Int, styleable: Int): Boolean {
+        if (ta.hasValue(styleable)) {
+            val layoutId = ta.getResourceId(styleable, 0)
+            addStateView(state, layoutId)
+            return true
+        }
+        return false
+    }
+
+    private fun addStateView(@FacedViewState state: Int, view: View) {
+        stateViews.put(state, view)
+        if (currentState != state) {
+            view.visibility = View.INVISIBLE
+        }
+        addView(view)
+    }
+
+    private fun addStateView(@FacedViewState state: Int, @LayoutRes layoutId: Int) {
+        val inflatedView = layoutInflater.inflate(layoutId, this, false)
+        addStateView(state, inflatedView)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T : View> getView(state: Int): T {
+        return stateViews.get(state) as T
+    }
+
+    fun hasStateViewAttached(): Boolean {
+        return childCount > 0
+    }
+
+    @FacedViewState
+    fun getState(): Int {
+        return this.currentState
+    }
+
+    fun setState(@FacedViewState state: Int) {
+        if (currentState == state) {
+            return
+        }
+
+        previousState = currentState
+        currentState = state
     }
 }
