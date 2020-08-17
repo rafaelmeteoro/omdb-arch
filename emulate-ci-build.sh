c0fbc737c884b5f98f709632b30f2ca32bd886ef
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2020-08-16 21:33:22 -0300

    Atualizado ci sh

--- emulate-ci-build.sh
+++ emulate-ci-build.sh
@@ -5,5 +5,5 @@ set -ex
 ./gradlew --stop clean
 ./gradlew ktlintCheck --no-daemon --stacktrace
 ./gradlew detekt --no-daemon --stacktrace
-#./gradlew test --no-daemon --stacktrace
-#./gradlew assembleDebug -xlint --no-daemon --stacktrace
+./gradlew test --no-daemon --stacktrace
+./gradlew assembleDebug -xlint --no-daemon --stacktrace
