6a792726b94b6442bd8c42439dfe1d5fc6818bb6
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2020-08-26 21:14:14 -0300

    Adicionado script para criar hash e atualizado workflow para ubuntu e rodar analysis

--- emulate-ci-build.sh
+++ emulate-ci-build.sh
@@ -5,5 +5,5 @@ set -ex
 ./gradlew --stop clean
 ./gradlew ktlintCheck --no-daemon --stacktrace
 ./gradlew detekt --no-daemon --stacktrace
-./gradlew jacocoTestReport jacocoTestReportDebug --no-daemon
+./gradlew jacocoTestReport jacocoTestReportDebug --no-daemon --stacktrace
 ./gradlew assembleDebug -xlint --no-daemon --stacktrace
